1cd550357b6446500e5b492e3d88445d5bf8f554
Garrett Pauls <garrett.pauls@gmail.com>
2019-01-05 11:30:45 -0700

    Added spacebar toggle for overlay UI

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,5 +1,7 @@
-use conrod_core::{widget, Widget, Sizeable, Colorable};
 use super::{ActionOverlay, ImageViewer};
+use conrod_core::{widget, Widget, Sizeable, Colorable};
+use conrod_core::event::{Event, Ui, Release, Button};
+use conrod_core::input::Key;
 
 widget_ids!(struct Ids {
     canvas,
@@ -9,6 +11,7 @@ widget_ids!(struct Ids {
 
 pub struct State {
     ids: Ids,
+    is_overlay_visible: bool,
 }
 
 #[derive(WidgetCommon)]
@@ -32,6 +35,7 @@ impl Widget for App {
     fn init_state(&self, id_gen: widget::id::Generator) -> Self::State {
         State {
             ids: Ids::new(id_gen),
+            is_overlay_visible: false,
         }
     }
 
@@ -45,6 +49,8 @@ impl Widget for App {
             ..
         } = args;
 
+        self.process_events(state, ui);
+
         widget::Canvas::new()
             .parent(id)
             .color(ui.theme.background_color)
@@ -56,11 +62,32 @@ impl Widget for App {
             .wh_of(id)
             .set(state.ids.viewer, ui);
 
-        for action in ActionOverlay::new()
-            .parent(id)
-            .wh_of(id)
-            .set(state.ids.overlay, ui) {
-            log::info!("overlay action: {:?}", action);
+        if state.is_overlay_visible {
+            for action in ActionOverlay::new()
+                .parent(id)
+                .wh_of(id)
+                .set(state.ids.overlay, ui) {
+                log::info!("overlay action: {:?}", action);
+            }
+        }
+    }
+}
+
+impl App {
+    fn process_events(&self, state: &mut widget::State<State>, ui: &mut conrod_core::UiCell) {
+        for event in ui.global_input().events().filter_map(|e| match e {
+            Event::Ui(ui) => Some(ui),
+            _ => None
+        }) {
+            match event {
+                Ui::Release(_, Release { button, .. }) => match button {
+                    Button::Keyboard(Key::Space) => {
+                        state.update(|s| s.is_overlay_visible = !s.is_overlay_visible);
+                    }
+                    _ => ()
+                },
+                _ => ()
+            }
         }
     }
 }
