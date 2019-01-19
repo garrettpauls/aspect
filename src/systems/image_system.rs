83599d490b8c68662452eddb9c817713c4e13e79
Garrett Pauls <garrett.pauls@gmail.com>
2019-01-19 00:07:46 -0700

    Updated main image loading to take place on a background thread

--- src/systems/image_system.rs
+++ src/systems/image_system.rs
@@ -3,6 +3,7 @@ use glium::Display;
 use glium::texture::{RawImage2d, SrgbTexture2d};
 use std::path::Path;
 use std::time::{Duration, Instant};
+use std::mem::replace;
 
 use crate::support::{ExtensionIs, ErrToString};
 use super::{EventSystem, AppEvent, events as e};
@@ -22,6 +23,7 @@ pub struct ImageSystem<'a> {
     current_frame: usize,
     last_update: Instant,
     display: &'a Display,
+    loader: Option<ImageLoader>,
 }
 
 impl<'a> ImageSystem<'a> {
@@ -32,6 +34,7 @@ impl<'a> ImageSystem<'a> {
             current_frame: 0,
             last_update: Instant::now(),
             display,
+            loader: None,
         }
     }
 
@@ -50,13 +53,23 @@ impl<'a> ImageSystem<'a> {
     }
 
     pub fn update(&mut self, events: &mut EventSystem) -> Result<(), String> {
-        let new_events: Vec<_> = events.events()
-            .filter_map(|event| match event {
+        for event in events.events() {
+            match event {
                 AppEvent::Load(file) => self.load_file(&file),
-                _ => None,
-            }).collect();
-        for event in new_events {
-            events.push(event);
+                _ => (),
+            }
+        }
+
+        if let Some(loader) = self.loader.take() {
+            let loader = loader.update();
+            match loader.try_get_image() {
+                Ok(result) => {
+                    if let Some(event) = self.finish_loader(result) {
+                        events.push(event);
+                    }
+                }
+                Err(loader) => self.loader = Some(loader),
+            }
         }
 
         if self.frames.len() > 1 {
@@ -90,44 +103,65 @@ impl<'a> ImageSystem<'a> {
         self.current_frame = 0;
     }
 
-    fn load_file(&mut self, path: &Path) -> Option<AppEvent> {
+    fn load_file(&mut self, path: &Path) {
         log::info!("Loading image from path: {}", path.display());
-        self.unload_image();
 
         if !path.exists() || !path.is_file() {
             log::error!("Could not load image from path which is not a file: {}", path.display());
-            return None;
+            self.unload_image();
+            return;
         }
 
-        let images = ImageLoader::load(path).log_err()?;
-
-        log::debug!("Converting raw frames to textures");
-        let mut frames = Vec::new();
-        for (raw, delay) in images {
-            let w = raw.width;
-            let h = raw.height;
-            if let Some(texture) = SrgbTexture2d::new(self.display, raw).log_err() {
-                frames.push(FrameData {
-                    id: self.image_map.insert(texture),
-                    w,
-                    h,
-                    delay,
-                });
+        if let Some(loader) = &self.loader {
+            if loader.is_loading_path(&path) {
+                return;
             }
         }
 
-        self.frames = frames;
-        self.current_frame = 0;
-        self.last_update = Instant::now();
+        let loader = ImageLoader::load(path.to_path_buf());
+        if let Some(old) = replace(&mut self.loader, Some(loader)) {
+            old.cancel();
+        }
+    }
 
-        if let Some(frame) = self.frames.get(0) {
-            Some(e::Image::Loaded {
-                id: frame.id,
-                w: frame.w,
-                h: frame.h,
-            }.into())
-        } else {
-            None
+    fn finish_loader(&mut self, result: Result<Vec<RawImageData>, String>) -> Option<AppEvent> {
+        log::info!("Unloading current frames");
+        self.unload_image();
+
+        match result {
+            Err(e) => {
+                log::error!("Failed to load image: {}", e);
+                None
+            }
+            Ok(images) => {
+                log::debug!("Converting raw frames to textures");
+                let mut frames = Vec::new();
+                for raw in images {
+                    let raw_image = RawImage2d::from_raw_rgba_reversed(&raw.buf, (raw.w, raw.h));
+                    if let Some(texture) = SrgbTexture2d::new(self.display, raw_image).log_err() {
+                        frames.push(FrameData {
+                            id: self.image_map.insert(texture),
+                            w: raw.w,
+                            h: raw.h,
+                            delay: raw.delay,
+                        });
+                    }
+                }
+
+                self.frames = frames;
+                self.current_frame = 0;
+                self.last_update = Instant::now();
+
+                if let Some(frame) = self.frames.get(0) {
+                    Some(e::Image::Loaded {
+                        id: frame.id,
+                        w: frame.w,
+                        h: frame.h,
+                    }.into())
+                } else {
+                    None
+                }
+            }
         }
     }
 
@@ -147,26 +181,90 @@ fn texture_from_image(display: &Display, image: image::DynamicImage) -> Result<(
         .err_to_string()
 }
 
-struct ImageLoader;
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::path::PathBuf;
+use std::convert::Into;
+
+enum ImageLoader {
+    Loading(PathBuf, Arc<Mutex<bool>>, thread::JoinHandle<Result<Vec<RawImageData>, String>>),
+    Completed(PathBuf, Result<Vec<RawImageData>, String>),
+}
+
+struct RawImageData {
+    buf: Vec<u8>,
+    w: u32,
+    h: u32,
+    delay: Duration,
+}
 
 impl ImageLoader {
-    pub fn load(path: &Path) -> Result<Vec<(RawImage2d<u8>, Duration)>, String> {
+    pub fn load(path: PathBuf) -> ImageLoader {
         if !path.exists() || !path.is_file() {
-            return Err(format!("Could not load image from path which is not a file: {}", path.display()));
+            let e = format!("Could not load image from path which is not a file: {}", path.display());
+            return ImageLoader::Completed(path, Err(e));
         }
 
-        if path.extension_is("gif") {
-            ImageLoader::load_gif(path)
-        } else {
-            ImageLoader::load_static_image(path)
+        let m = Arc::new(Mutex::new(false));
+        let thread_path = path.clone();
+        ImageLoader::Loading(path, m.clone(), thread::spawn(move || {
+            let result = if thread_path.extension_is("gif") {
+                ImageLoader::load_gif(&thread_path)
+            } else {
+                ImageLoader::load_static_image(&thread_path)
+            };
+
+            log::debug!("Loading image complete, notifying waiting threads");
+            let mut completed = m.lock().unwrap();
+            *completed = true;
+
+            log::debug!("Loading image complete, exiting thread");
+            result
+        }))
+    }
+
+    pub fn is_loading_path(&self, target: &Path) -> bool {
+        match self {
+            ImageLoader::Completed(path, _) => path == target,
+            ImageLoader::Loading(path, _, _) => path == target,
         }
     }
 
-    fn load_gif(path: &Path) -> Result<Vec<(RawImage2d<u8>, Duration)>, String> {
+    pub fn update(self) -> Self {
+        match self {
+            ImageLoader::Loading(path, arc, handle) => {
+                log::debug!("Updating loading image loader");
+                if *arc.lock().unwrap() {
+                    log::debug!("Loading complete, joining thread");
+                    let result = handle.join().unwrap();
+                    ImageLoader::Completed(path, result)
+                } else {
+                    log::debug!("Loading not complete");
+                    ImageLoader::Loading(path, arc, handle)
+                }
+            }
+            _ => self,
+        }
+    }
+
+    pub fn try_get_image(self) -> Result<Result<Vec<RawImageData>, String>, Self> {
+        match self {
+            ImageLoader::Completed(_, result) => Ok(result),
+            _ => Err(self),
+        }
+    }
+
+    pub fn cancel(self) {
+        log::info!("Cancelling load of image");
+        // TODO: properly dispose of the thread
+    }
+
+    fn load_gif(path: &Path) -> Result<Vec<RawImageData>, String> {
         use gif::Decoder;
         use gif_dispose::{Screen, RGBA8};
         use std::fs::File;
 
+        log::debug!("Loading file as gif: {}", path.display());
         let file = File::open(path).err_to_string()?;
         let decoder = Decoder::new(file);
         let mut reader = decoder.read_info().err_to_string()?;
@@ -184,7 +282,7 @@ impl ImageLoader {
                 continue;
             }
 
-            let mut buf = Vec::new();
+            let mut buf = Vec::with_capacity(screen.pixels.buf.len() * 4);
             for p in &screen.pixels.buf {
                 buf.push(p.r);
                 buf.push(p.g);
@@ -192,9 +290,14 @@ impl ImageLoader {
                 buf.push(p.a);
             }
 
-            let raw = RawImage2d::from_raw_rgba_reversed(&buf, (w, h));
+            let raw = RawImageData {
+                buf,
+                w,
+                h,
+                delay: Duration::from_millis(frame.delay as u64 * 10),
+            };
 
-            frames.push((raw, Duration::from_millis(frame.delay as u64 * 10)));
+            frames.push(raw);
         }
 
         if frames.is_empty() {
@@ -204,12 +307,19 @@ impl ImageLoader {
         }
     }
 
-    fn load_static_image(path: &Path) -> Result<Vec<(RawImage2d<u8>, Duration)>, String> {
+    fn load_static_image(path: &Path) -> Result<Vec<RawImageData>, String> {
+        log::debug!("Loading file as static image: {}", path.display());
+
         let image = image::open(path).err_to_string()?;
         let rgba = image.to_rgba();
         let dimensions = rgba.dimensions();
-        let raw = RawImage2d::from_raw_rgba_reversed(&rgba.into_raw(), dimensions);
+        let raw = RawImageData {
+            buf: rgba.into_raw(),
+            w: dimensions.0,
+            h: dimensions.1,
+            delay: Duration::default(),
+        };
 
-        Ok(vec![(raw, Duration::default())])
+        Ok(vec![raw])
     }
 }
