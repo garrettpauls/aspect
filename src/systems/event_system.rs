d436f2e88f7da856cbd6e78e237cb92814048389
Garrett Pauls <garrett.pauls@gmail.com>
2019-01-18 21:59:39 -0700

    Updated event system to be threadsafe around queueing events

--- src/systems/event_system.rs
+++ src/systems/event_system.rs
@@ -1,29 +1,31 @@
 use super::events::AppEvent;
 
 use std::slice::Iter;
+use std::sync::Mutex;
 
 pub struct EventSystem {
     current: Vec<AppEvent>,
-    pending: Vec<AppEvent>,
+    pending: Mutex<Vec<AppEvent>>,
 }
 
 impl EventSystem {
     pub fn new() -> Self {
         EventSystem {
             current: Vec::new(),
-            pending: Vec::new(),
+            pending: Mutex::new(Vec::new()),
         }
     }
 
     pub fn push(&mut self, event: AppEvent) {
         log::trace!("Queuing event: {:?}", event);
-        self.pending.push(event);
+        self.pending.lock().unwrap().push(event);
     }
 
     pub fn events(&self) -> Iter<AppEvent> { self.current.iter() }
 
     pub fn update(&mut self) {
         use std::mem::replace;
-        self.current = replace(&mut self.pending, Vec::new());
+        let mut pending = self.pending.lock().unwrap();
+        self.current = replace(&mut *pending, Vec::new());
     }
 }
